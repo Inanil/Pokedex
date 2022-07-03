7242f5a6405f70c47d0066a4332de4cb8c74966a
Paulina Flores <modsmith2@gmail.com>
2022-07-02 23:59:18 -0500

    finally

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Gaming1 from '../../assets/img/gaming1.png'
+import Ash from '../../assets/img/ash.png'
+
+const Header = () => {
+  return (
+    <header className='header'> 
+       
+        <img src={Gaming1} alt="" />
+        <img src={Ash} alt="" />
+       </header>
+  )
+}
+
+export default Header
